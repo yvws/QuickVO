db57ac915280a83c7216f5072f6177ac190b8c90
sq-0523 <>
2024-10-12 15:15:35 +0800

    init

---
+++ Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "QuickVO",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(
+            name: "QuickVO",
+            targets: ["QuickVO"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/daltoniam/Starscream.git", from: "4.0.8"),
+        .package(url: "https://github.com/stasel/WebRTC", from: "129.0.0")
+    ],
+    targets: [
+        .binaryTarget(name: "QuickVO", path: "QuickVO.xcframework"),
+    ]
+
+
+
+)
---
+++ QuickVO.xcframework/ios-arm64/QuickVO.framework/Headers/QuickVO.h
@@ -0,0 +1,18 @@
+//
+//  QuickVO.h
+//  QuickVO
+//
+//  Created by s-0523 on 2024/9/29.
+//
+
+#import <Foundation/Foundation.h>
+#import <QuickVO/RTCBeautyFilter.h>
+//! Project version number for QuickVO.
+FOUNDATION_EXPORT double QuickVOVersionNumber;
+
+//! Project version string for QuickVO.
+FOUNDATION_EXPORT const unsigned char QuickVOVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <QuickVO/PublicHeader.h>
+
+
