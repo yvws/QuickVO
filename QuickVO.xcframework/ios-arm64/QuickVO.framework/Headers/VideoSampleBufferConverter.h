8cb7ca84b213bb6cd666ba2070dfa7c215433591
sq-0523 <sq-0523>
2025-01-23 14:38:55 +0800

    fet: pip

--- QuickVO.xcframework/ios-arm64/QuickVO.framework/Headers/VideoSampleBufferConverter.h
+++ QuickVO.xcframework/ios-arm64/QuickVO.framework/Headers/VideoSampleBufferConverter.h
@@ -2,13 +2,13 @@
 
 #import <Foundation/Foundation.h>
 #import <ReplayKit/ReplayKit.h>
-
+#import <WebRTC/WebRTC.h>
 NS_ASSUME_NONNULL_BEGIN
 
 @interface VideoSampleBufferConverter : NSObject
 
 + (void)PixelBuffer:(int )width height:(int )height data:(NSData *)data block:(void(^)(CVPixelBufferRef ))cb;
-
++ (void)pixelBufferWithI420Buffer:(RTCI420Buffer *)buffer block:(void(^)(CVPixelBufferRef ))cb;
 @end
 
 NS_ASSUME_NONNULL_END
