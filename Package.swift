3c6f3ef8d3d8c22429921097daef1d05e52ecd53
sq-0523 <sq-0523>
2025-07-01 15:07:51 +0800

    feat backgroudAudio

--- Package.swift
+++ Package.swift
@@ -20,7 +20,8 @@ let package = Package(
         .package(url: "https://github.com/apple/swift-protobuf.git", from: "1.28.2"),
         .package(url: "https://github.com/1024jp/GzipSwift", from: "6.0.0"),
         .package(url: "https://github.com/motian30/GPUPixelLib.git", from: "1.0.2"),
-        .package(url: "https://github.com/SwiftyBeaver/SwiftyBeaver.git", from: "2.1.1")
+        .package(url: "https://github.com/SwiftyBeaver/SwiftyBeaver.git", from: "2.1.1"),
+        .package(url: "https://github.com/alta/swift-opus.git", from: "0.0.2")
     
     ],
     targets: [
@@ -33,6 +34,7 @@ let package = Package(
             .product(name: "GPUPixelLib", package: "GPUPixelLib", condition: .when(platforms: [.iOS])),
             .product(name: "SwiftProtobuf", package: "swift-protobuf"),
             .product(name: "Gzip", package: "GzipSwift"),
+            .product(name: "Opus", package: "swift-opus"),
 
         ]),
           
