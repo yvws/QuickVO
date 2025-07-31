a1e23dfdd1a945af90037a6a3b1b2bccb2bda3b9
sq-0523 <sq-0523>
2025-07-31 11:11:19 +0800

    refactor audio

--- Package.swift
+++ Package.swift
@@ -16,13 +16,11 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/daltoniam/Starscream.git", from: "4.0.8"),
-        .package(url: "https://github.com/stasel/WebRTC", from: "132.0.0"),
+        .package(url: "https://github.com/stasel/WebRTC", from: "137.0.0"),
         .package(url: "https://github.com/apple/swift-protobuf.git", from: "1.28.2"),
         .package(url: "https://github.com/1024jp/GzipSwift", from: "6.0.0"),
         .package(url: "https://github.com/motian30/GPUPixelLib.git", from: "1.0.2"),
         .package(url: "https://github.com/SwiftyBeaver/SwiftyBeaver.git", from: "2.1.1"),
-        .package(url: "https://github.com/alta/swift-opus.git", from: "0.0.2")
-    
     ],
     targets: [
         .binaryTarget(name: "QuickVO", path: "QuickVO.xcframework"),
@@ -34,7 +32,6 @@ let package = Package(
             .product(name: "GPUPixelLib", package: "GPUPixelLib", condition: .when(platforms: [.iOS])),
             .product(name: "SwiftProtobuf", package: "swift-protobuf"),
             .product(name: "Gzip", package: "GzipSwift"),
-            .product(name: "Opus", package: "swift-opus"),
 
         ]),
           
